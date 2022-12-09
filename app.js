1c2c949171b1e868d026fd1251b5c645d9cc1141
Makushenko Oleksii <115660359+Makushenko-Oleksii@users.noreply.github.com>
2022-12-09 15:58:37 +0200

    Add files via upload
    fix

--- app.js
+++ app.js
@@ -1,76 +1,70 @@
-const userInfo = document.querySelector('.user-info');
-const userImg = document.querySelector('.user-img img');
-const userFollower = document.querySelector('.user-follower')
-const dateCreation = document.querySelector('.date-creation');
-const repositories = document.querySelector('.repositories');
 
-const showUser = (user) => {
-    userInfo.innerHTML = `${user.login}`;
-    userFollower.innerHTML = `${user.followers} follower  ${user.following} following`;
-    userImg.src = `${user.avatar_url}`;
-    dateCreation.textContent = `${new Date(user.created_at).toLocaleString()}`;
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    setTimeout(()=>{
-        const loader = document.querySelector('.loader');
-        const container = document.querySelector('.container');
-        container.classList.remove('none')
-        loader.classList.add('none')
-    },2000)
+fetch("https://api.github.com/users/Makushenko-Oleksii").then(data => {
+    document.querySelector('.loader-mask').classList.remove('none');
+    if(data.ok) {
+        document.querySelector('.loader-mask').classList.add('none');
+    }
 })
 
+document.addEventListener('DOMContentLoaded', init);
 
-document.addEventListener('DOMContentLoaded', function () {
-    fetch("https://api.github.com/users/Makushenko-Oleksii").then(response => {
+const FETCH_URL = ("https://api.github.com/users/Makushenko-Oleksii");
+const url = 'https://api.github.com/users/Makushenko-Oleksii/repos';
+function init() {
+    const userInfo = document.querySelector('.js-user-info');
+    const userImg = document.querySelector('.js-user-img img');
+    const userFollower = document.querySelector('.js-user-follower')
+    const dateCreation = document.querySelector('.js-date-creation');
+
+    fetch(FETCH_URL).then(response => {
         return response.json();
     }).then((json) => {
         showUser(json)
-    })
-});
+    });
 
-let url = 'https://api.github.com/users/Makushenko-Oleksii/repos';
+    const showUser = (user) => {
+        userInfo.innerHTML = `${user.login}`;
+        userFollower.innerHTML = `${user.followers} follower  ${user.following} following`;
+        userImg.src = `${user.avatar_url}`;
+        dateCreation.textContent = `${new Date(user.created_at).toLocaleString()}`;
+    }
 
-document.addEventListener('DOMContentLoaded', getRepos);
+    function getRepos(url) {
+        fetch(url).then(response => {
+            return response.json();
+        }).then((json) => {
+            outRepos(json)
+        })
+    }
 
-function getRepos(url) {
-    fetch(url).then(response => {
-        return response.json();
-    }).then((json) => {
-        outRepos(json)
-    })
-}
+    getRepos(url);
 
-getRepos(url);
+    function outRepos(info) {
+        let out = document.querySelector('.out');
+        info.forEach(elem => {
+            const name = document.createElement('p');
+            name.classList.add('name', 'fw-bolder');
+            name.innerHTML = `имя: ${elem.name}`
+            out.append(name);
+            name.addEventListener('click', function () {
+                const branch = document.createElement('p');
+                branch.classList.add('branch', 'mt-3');
+                name.append(branch);
+                branch.innerHTML = `ветка: ${elem.default_branch}`;
+                const visibility = document.createElement('p');
+                visibility.classList.add('visibility');
+                name.append(visibility);
+                visibility.innerHTML = `видимость: ${elem.visibility}`;
+                const issuesOpen = document.createElement('p');
+                issuesOpen.classList.add('open_issues');
+                name.append(issuesOpen);
+                issuesOpen.innerHTML = `open issues: ${elem.open_issues}`;
+                const pushAt = document.createElement('p');
+                pushAt.classList.add('push-at');
+                name.append(pushAt);
+                pushAt.innerHTML = `дата последнего изменения: ${new Date(elem.pushed_at).toLocaleString()}`;
 
-function outRepos(info) {
-    console.log(info)
-
-    let out = document.querySelector('.out');
-    info.map(elem => {
-        const name = document.createElement('p');
-        name.classList.add('name', 'fw-bolder');
-        name.innerHTML = `имя: ${elem.name}`
-        out.append(name);
-        name.addEventListener('click', function () {
-            const branch = document.createElement('p');
-            branch.classList.add('branch', 'mt-3');
-            name.append(branch);
-            branch.innerHTML = `ветка: ${elem.default_branch}`;
-            const visibility = document.createElement('p');
-            visibility.classList.add('visibility');
-            name.append(visibility);
-            visibility.innerHTML = `видимость: ${elem.visibility}`;
-            const issuesOpen = document.createElement('p');
-            issuesOpen.classList.add('open_issues');
-            name.append(issuesOpen);
-            issuesOpen.innerHTML = `open issues: ${elem.open_issues}`;
-            const pushAt = document.createElement('p');
-            pushAt.classList.add('push-at');
-            name.append(pushAt);
-            pushAt.innerHTML = `дата последнего изменения: ${ new Date(elem.pushed_at).toLocaleString()}`;
-
-        }, {once:true})
-
-    })
+            }, {once: true})
+        })
+    }
 }
