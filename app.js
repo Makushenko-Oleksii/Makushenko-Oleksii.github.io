27c3b678e53f47cb01d2631c123384317b2fd056
Makushenko Oleksii <makushenkooleksii@gmail.com>
2024-11-30 23:57:11 +0200

    audio_player_change

--- app.js
+++ app.js
@@ -2,15 +2,17 @@
 const audio = document.getElementById('audio');
 const playPauseBtn = document.querySelector('.play-pause-btn');
 const progressBar = document.querySelector('.progress-bar');
+const volumeBtn = document.querySelector('.volume-btn');
+const volumeControl = document.querySelector('.volume-control');
 
 // Функция воспроизведения/паузы
 playPauseBtn.addEventListener('click', () => {
     if (audio.paused) {
         audio.play();
-        playPauseBtn.innerHTML = '<img src="/images/play.png" alt="Pause" width="21" height="22">' // Меняем иконку
+        playPauseBtn.innerHTML = '<img src="/images/pause.png" alt="Pause" width="21" height="22">';
     } else {
         audio.pause();
-        playPauseBtn.innerHTML = '<img src="/images/pause.png" alt="Pause" width="21" height="22">';
+        playPauseBtn.innerHTML = '<img src="/images/play.png" alt="Play" width="21" height="22">';
     }
 });
 
@@ -22,4 +24,15 @@ audio.addEventListener('timeupdate', () => {
 // Перемотка трека
 progressBar.addEventListener('input', () => {
     audio.currentTime = (progressBar.value / 100) * audio.duration;
+});
+
+
+// Установка громкости по умолчанию
+audio.volume = 0.5; // Громкость аудио 50%
+volumeControl.value = 0.5; // Устанавливаем ползунок громкости на середину
+
+
+// Регулировка громкости
+volumeControl.addEventListener('input', (event) => {
+    audio.volume = event.target.value; // Устанавливаем громкость от 0 до 1
 });
