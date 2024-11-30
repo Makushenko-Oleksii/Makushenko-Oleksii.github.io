e4560442b48c0c0073aef3ad1269b675690fa55e
Makushenko Oleksii <makushenkooleksii@gmail.com>
2024-12-01 00:18:51 +0200

    audio_player_change_volume

--- app.js
+++ app.js
@@ -2,8 +2,13 @@
 const audio = document.getElementById('audio');
 const playPauseBtn = document.querySelector('.play-pause-btn');
 const progressBar = document.querySelector('.progress-bar');
-const volumeBtn = document.querySelector('.volume-btn');
 const volumeControl = document.querySelector('.volume-control');
+const volumePercentage = document.querySelector('.volume-percentage');
+
+// Установка громкости по умолчанию
+audio.volume = 0.5; // Громкость аудио 50%
+volumeControl.value = 0.5; // Устанавливаем ползунок громкости на середину
+volumePercentage.textContent = '50%'; // Отображаем 50%
 
 // Функция воспроизведения/паузы
 playPauseBtn.addEventListener('click', () => {
@@ -26,13 +31,9 @@ progressBar.addEventListener('input', () => {
     audio.currentTime = (progressBar.value / 100) * audio.duration;
 });
 
-
-// Установка громкости по умолчанию
-audio.volume = 0.5; // Громкость аудио 50%
-volumeControl.value = 0.5; // Устанавливаем ползунок громкости на середину
-
-
 // Регулировка громкости
 volumeControl.addEventListener('input', (event) => {
     audio.volume = event.target.value; // Устанавливаем громкость от 0 до 1
+    const volumePercent = Math.round(event.target.value * 100); // Преобразуем в проценты
+    volumePercentage.textContent = `${volumePercent}%`; // Обновляем текст
 });
